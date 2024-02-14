8ec96d237706ac9ab2ead593c93030b255528f3d
e-tsoni <etsoni.cs@gmail.com>
2024-02-14 20:24:48 +0200

    ContourDetection class (DONE))

--- edge_and_contour_detection/ContourDetection/ContourDetection.py
+++ edge_and_contour_detection/ContourDetection/ContourDetection.py
@@ -1,5 +1,34 @@
 import cv2
 
 
-if __name__ == "__main__":
-    pass
+class ContourDetection:
+    def __init__(self, edged, or_img, parameters_contour):
+        self.edged = edged
+        self.image = or_img
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        self.mode = parameters_contour['mode']
+        self.approx_mode = parameters_contour['approx_mode']
+        if parameters_contour['kernel'] == 1:
+            self.kernel = cv2.getStructuringElement(cv2.MORPH_RECT, parameters_contour['ksize'])
+        elif parameters_contour['kernel'] == 2:
+            self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, parameters_contour['ksize'])
+        elif parameters_contour['kernel'] == 3:
+            self.kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, parameters_contour['ksize'])
+        else:
+            print("Invalid kernel")
+        self.iterations = parameters_contour['iterations']
+
+    def get_or_img(self):
+        return self.image
+
+    def get_edged(self):
+        return self.edged
+
+    def find_contours(self):
+        dilate = cv2.dilate(self.edged, self.kernel, iterations=self.iterations)
+        contours, _ = cv2.findContours(dilate, self.mode, self.approx_mode)
+        cv2.drawContours(self.image, contours, -1, (0, 255, 0), 2)
+        print(len(contours), "objects were found in this image.")
+        cv2.imshow("Edged image", self.edged)
+        cv2.imshow("contours", self.image)
+        cv2.waitKey(0)
--- edge_and_contour_detection/edge_and_contour_detection.py
+++ edge_and_contour_detection/edge_and_contour_detection.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from ImageProperties.ImageProperties import ImageProperties
 from EdgeDetection.EdgeDetection import EdgeDetection
-
+from ContourDetection.ContourDetection import ContourDetection
 
 if __name__ == "__main__":
 
@@ -20,20 +20,31 @@ if __name__ == "__main__":
     edged_img_path = os.path.join(imgs_path, 'edged_image.jpeg')
 
     ############# PARAMETERS DEFINED BY USER ###########################################################################
-    parameters_dic = {
+    parameters_edge = {
         # method: 1 for Sobel, 2 for Canny
         'method': 2,
         'ksize': 3,  # only for Sobel method (default 3)
-        'thresholds': (50, 100)  # only for Canny method (default (50, 100))
+        'thresholds': (100, 200)  # only for Canny method (default (50, 100))
+    }
+
+    parameters_contour = {
+        'mode': 1,  # 1=cv2.RETR_EXTERNAL, 2=cv2.RETR_LIST, 3=cv2.RETR_CCOMP, 4=cv2.RETR_TREE, 5=cv2.RETR_FLOODFILL
+        'approx_mode': 2,  # 1=cv.CHAIN_APPROX_NONE, 2=cv.CHAIN_APPROX_SIMPLE, 3=cv.CHAIN_APPROX_TC89_L1,
+        # 4=cv.CHAIN_APPROX_TC89_KCOS
+        'kernel': 1,  # 1=cv2.MORPH_RECT, 2=cv2.MORPH_ELLIPSE, 3=cv2.MORPH_CROSS
+        'ksize': (5, 5),
+        'iterations': 5  # number of iterations
     }
 
     ############ EDGE DETECTION ########################################################################################
     image_object = ImageProperties(image_path)
     print(image_object.image_properties())
-
-    edge_detection_object = EdgeDetection(image_object, parameters_dic)
+    edge_detection_object = EdgeDetection(image_object, parameters_edge)
     edged = edge_detection_object.edge_detection()
-
+    # save edged image
     img_to_save = Image.fromarray(edged)
-    edged_img_path = os.path.join(imgs_path, 'edged_image.jpeg')
     img_to_save.save(edged_img_path)
+
+    ########### CONTOUR DETECTION ######################################################################################
+    contour_detection_object = ContourDetection(edged, image_object.image, parameters_contour)
+    contour_detection_object.find_contours()
