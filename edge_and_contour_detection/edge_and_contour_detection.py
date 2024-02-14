e5269bbf2ab310a5758732d791086e9236d7a930
e-tsoni <etsoni.cs@gmail.com>
2024-02-14 18:20:39 +0200

    EdgeDetection Class (Sobel and Canny)

--- edge_and_contour_detection/edge_and_contour_detection.py
+++ edge_and_contour_detection/edge_and_contour_detection.py
@@ -1,36 +1,39 @@
-# import sys as s
+import sys as s
 import os
 import cv2
-
+from PIL import Image
+import numpy as np
 from pathlib import Path
+
 from ImageProperties.ImageProperties import ImageProperties
 from EdgeDetection.EdgeDetection import EdgeDetection
 
 
 if __name__ == "__main__":
 
-    # for key in s.modules:
-    #     print(key)
+    ############## PATHS FOR THE IMAGES ################################################################################
 
     # project_path = os.getcwd() # The folder of the project (root)
     project_path = Path(__file__).parent.parent  # The folder of the project (root)
-    print(project_path)
     imgs_path = os.path.join(project_path, 'imgs', 'test_imgs')
-    print(imgs_path)
     image_path = os.path.join(imgs_path, "office-desk-workspace-table-background.jpeg")
-    print(image_path)
+    edged_img_path = os.path.join(imgs_path, 'edged_image.jpeg')
 
-    # image = cv2.imread(image_path)
+    ############# PARAMETERS DEFINED BY USER ###########################################################################
+    parameters_dic = {
+        # method: 1 for Sobel, 2 for Canny
+        'method': 2,
+        'ksize': 3,  # only for Sobel method (default 3)
+        'thresholds': (50, 100)  # only for Canny method (default (50, 100))
+    }
+
+    ############ EDGE DETECTION ########################################################################################
     image_object = ImageProperties(image_path)
     print(image_object.image_properties())
 
-    # gray_image = image_object.color_to_grayscale()
-    # print(gray_image)
+    edge_detection_object = EdgeDetection(image_object, parameters_dic)
+    edged = edge_detection_object.edge_detection()
 
-    edge_detection_object = EdgeDetection(image_object)
-
-    edged = edge_detection_object.edge_detection(50, 200)
-
-    cv2.imshow('edge', edged)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    img_to_save = Image.fromarray(edged)
+    edged_img_path = os.path.join(imgs_path, 'edged_image.jpeg')
+    img_to_save.save(edged_img_path)
